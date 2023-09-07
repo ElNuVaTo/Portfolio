a4bf0708c74183fafc4e96df490da9a5308a7e04
ElNuVaTo <amaroalcaq23@gmail.com>
2023-09-06 21:42:01 -0300

    fix: con componentes

--- src/pages/InicioPage.jsx
+++ src/pages/InicioPage.jsx
@@ -1,55 +1,54 @@
-// import { styled } from "styled-components";
+import { styled } from "styled-components";
 
-// import SobreMi from "@/components/SobreMi/SobreMi";
-// import Contacto from "@/components/Contacto/Contacto";
+import SobreMi from "@/components/SobreMi/SobreMi";
+import Contacto from "@/components/Contacto/Contacto";
 
-// import Dominado from "@/components/Habilidades/Dominado";
-// import Spotify from "@/components/Spotify/Spotify";
-// import Aprendiendo from "@/components/Habilidades/Aprendiendo";
+import Dominado from "@/components/Habilidades/Dominado";
+import Spotify from "@/components/Spotify/Spotify";
+import Aprendiendo from "@/components/Habilidades/Aprendiendo";
 
-// import Proyectos from "@/components/Proyectos/Proyectos";
-// import A_Uno from "@/components/Herramientas/a_Uno";
-// import B_Dos from "@/components/Herramientas/B_Dos";
-// import C_Tres from "@/components/Herramientas/C_Tres";
-// import Enlaces from "@/components/Proyectos/Enlaces";
+import Proyectos from "@/components/Proyectos/Proyectos";
+import A_Uno from "@/components/Herramientas/a_Uno";
+import B_Dos from "@/components/Herramientas/B_Dos";
+import C_Tres from "@/components/Herramientas/C_Tres";
+import Enlaces from "@/components/Proyectos/Enlaces";
 
-// const Main = styled.main`
-//   display: flex;
-//   flex-direction: column;
-//   padding: 35px;
-//   gap: 35px;
-// `;
-// const AboutMe = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   gap: 40px;
-//   flex-wrap: wrap;
-// `;
-// const SpotifyAndSkills = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   gap: 40px;
-//   flex-wrap: wrap;
-// `;
-// const ProyectosAndEnlaces = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   flex-wrap: wrap-reverse;
-//   gap: 40px;
-// `;
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  padding: 35px;
+  gap: 35px;
+`;
+const AboutMe = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 40px;
+  flex-wrap: wrap;
+`;
+const SpotifyAndSkills = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 40px;
+  flex-wrap: wrap;
+`;
+const ProyectosAndEnlaces = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap-reverse;
+  gap: 40px;
+`;
 
-// const Herramientas = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   gap: 40px;
-//   flex-wrap: wrap;
-// `;
+const Herramientas = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 40px;
+  flex-wrap: wrap;
+`;
 
 const InicioPage = () => {
   return (
     <>
-    <button>Test sin ningun componente</button>
-      {/* <Main>
+      <Main>
         <AboutMe>
           <SobreMi />
           <Contacto />
@@ -71,7 +70,7 @@ const InicioPage = () => {
           <B_Dos />
           <C_Tres />
         </Herramientas>
-      </Main> */}
+      </Main>
     </>
   );
 };
