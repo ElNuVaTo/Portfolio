5e16518c678149891d57382c42ae1900de9b7a4c
ElNuVaTo <amaroalcaq23@gmail.com>
2023-09-06 21:34:03 -0300

    fix: test sin ningun componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import InicioPage from "./pages/InicioPage";
+import InicioPage from "@/pages/InicioPage";
 
 const App = () => {
   return (
     <>
       <BrowserRouter basename="/PortafolioV5">
-        <Routes>{/* <Route path="/" element={<InicioPage />} /> */}</Routes>
+        <Routes>
+          <Route path="/" element={<InicioPage />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
